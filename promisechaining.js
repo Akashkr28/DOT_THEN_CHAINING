e9f6d995e52d45c377a8849b8a03dbe4f4a93961
Akashkr28 <email.akash2010@gmail.com>
2024-05-05 17:14:14 +0530

    update

--- promisechaining.js
+++ promisechaining.js
@@ -6,22 +6,17 @@ function createPromise(time){
     });
 }
 
-const resolver = (val) => {
-    console.log("Resolving with", val);
-    return 20
-};
-const rejector = (val) => console.log("Rejecting with", val);
 
-createPromise(10000)
+createPromise(10000) // returb pr1 {value: undefined, state: pending, onFulfilled: [f], onRejected: []}
 .then((value) => {
     console.log("Value in first then", value);
     return value*2;
-})
+}) //return pr2 {value: undefined, state: pending, onFulfilled: [g], onRejected: []}
 .then((value) => {
     console.log("Value in second then", value);
     return value*2;
-})
+}) //return pr3 {value: undefined, state: pending, onFulfilled: [h], onRejected: []}
 .then((value) => {
     console.log("Value in third then", value);
     return value*2;
-});
+}); //return pr4 {value: undefined, state: pending, onFulfilled: [h], onRejected: []}
